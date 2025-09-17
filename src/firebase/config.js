82d315e197b9d2dc2b412f431f16935e7103826c
cqiu-dot <cqiu@college.harvard.edu>
2025-09-17 15:19:42 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,16 +2,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBffq1ANXUapIjK-wG2yGFwg2-44e3A8Pc",
-  authDomain: "hpair-deliv-6443a.firebaseapp.com",
-  projectId: "hpair-deliv-6443a",
-  storageBucket: "hpair-deliv-6443a.firebasestorage.app",
-  messagingSenderId: "908480646127",
-  appId: "1:908480646127:web:e8861bd5881b714a4d041a",
-  measurementId: "G-5KKED2YT25"
+  apiKey: "AIzaSyBc95OESB-RN5XZEEvvkiDnGi7Yp5lM4xs",
+  authDomain: "hpair-deliverable.firebaseapp.com",
+  projectId: "hpair-deliverable",
+  storageBucket: "hpair-deliverable.firebasestorage.app",
+  messagingSenderId: "605325142193",
+  appId: "1:605325142193:web:bcb054b82464fe9a6c9a90",
+  measurementId: "G-6TW9K7XQ5B"
 };
 
 // Initialize Firebase
@@ -22,5 +23,8 @@ export const db = getFirestore(app);
 
 // Initialize Auth
 export const auth = getAuth(app);
+
+// Initialize Analytics
+const analytics = getAnalytics(app);
 
 export default app;
