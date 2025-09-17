8328c1f3a656f7a5fe9fa09809d959ac1e37e08f
cqiu-dot <cqiu@college.harvard.edu>
2025-09-17 16:44:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,37 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
+  const [values, setValues] = useState({
+    fullname: '',
+    email: '',
+    phonenumber: '',
+    address: '',
+    nationality: '',
+    language: '',
+    subject: '',
+    cv: '',
+    linkedin: ''
+  })
+  const handleChanges = (e) => {
+    setValues({...Values, [e.target.name]:[e.target.value]})
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(values)
+  }
+  const handleReset = () => {
+    setValues({
+      fullname: '',
+      email: '',
+      phonenumber: '',
+      address: '',
+      nationality: '',
+      language: '',
+      subject: '',
+      cv: '',
+      linkedin: ''
+    })
+  }
   return (
     <AuthProvider>
       <Router>
@@ -34,27 +65,33 @@ function App() {
             <h1>Personal Information Form Challenge</h1>
           </header>
           <main>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label htmlFor="fullname">Full Name*</label>
-              <input type="text" placeholder="Your answer" name="fullname"/>
+              <input type="text" placeholder="Your answer" name="fullname"
+              onChange={(e) => handleChanges(e)} required value={values.firstname}/>
     
               <label htmlFor="email">Email*</label>
-              <input type="email" placeholder="Your answer" name="email"/>
+              <input type="email" placeholder="Your answer" name="email"
+              onChange={(e) => handleChanges(e)} required value={values.email}/>
     
               <label htmlFor="phonenumber">Phone Number*</label>
-              <input type="text" placeholder="Your answer" name="phonenumber"/>
+              <input type="text" placeholder="Your answer" name="phonenumber"
+              onChange={(e) => handleChanges(e)} required value={values.phonenumber}/>
     
               <label htmlFor="address">Full Address*</label>
-              <input type="text" placeholder="Your answer" name="address"/>
+              <input type="text" placeholder="Your answer" name="address"
+              onChange={(e) => handleChanges(e)} required value={values.address}/>
     
               <label htmlFor="nationality">Nationality*</label>
-              <input type="text" placeholder="Your answer" name="nationality"/>
+              <input type="text" placeholder="Your answer" name="nationality"
+              onChange={(e) => handleChanges(e)} required value={values.nationality}/>
     
               <label htmlFor="language">Preferred Language</label>
-              <input type="text" placeholder="Your answer" name="language"/>
+              <input type="text" placeholder="Your answer" name="language"
+              onChange={(e) => handleChanges(e)} required value={values.language}/>
 
               <label hmtFor="subject">Research Interests</label>
-              <select name="subject" id="subject">
+              <select name="subject" id="subject" onChange={(e) => handleChanges(e)}/>
                 <option value="abio">Animal Sciences (ABIO)</option>
                 <option value="beha">Behavioral Sciences (BEHA)</option>
                 <option value="bchm">Biochemistry (BCHM)</option>
@@ -73,15 +110,16 @@ function App() {
               </select>
 
               <label htmlFor="cv">CV (one-page)*</label>
-              <input type="file" placeholder="Select File" name="cv"/>
+              <input type="file" placeholder="Select File" name="cv"
+              onChange={(e) => handleChanges(e)} required value={values.cv}/>
     
-              <label htmlFor="language">LinkedIn URL</label>
-              <input type="text" placeholder="Your answer" name="linkedin"/>
+              <label htmlFor="linkedin">LinkedIn URL</label>
+              <input type="text" placeholder="Your answer" name="linkedin"
+              onChange={(e) => handleChanges(e)}/>
 
-              <button type="button">Reset</button>
-              <button type="submit>Submit</button>
-
-              
+              <button type="button" onClick={handleReset}>Reset</button>
+              <button type="submit">Submit</button>
+                
             <\form>
             <Routes>
               <Route path="/" element={
