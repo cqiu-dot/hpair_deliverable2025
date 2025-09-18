589da9a8bc7ac7709341bd1cc4947a5342107ba8
cqiu-dot <cqiu@college.harvard.edu>
2025-09-17 20:35:20 -0400

    Update PersonalInfoStep.js

--- src/components/steps/PersonalInfoStep.js
+++ src/components/steps/PersonalInfoStep.js
@@ -1,68 +1,55 @@
 import React from 'react';
+import { Field, ErrorMessage } from 'formik';
 
-const PersonalInfoStep = ({ formData, setFormData }) => {
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prev => ({
-      ...prev,
-      [name]: value
-    }));
-  };
-
+const PersonalInfoStep = () => {
   return (
     <div>
       <h2>Personal Information</h2>
       <p>Please provide your basic personal details.</p>
       
       <div className="form-group">
-        <label className="form-label">First Name *</label>
-        <input 
-          type="text" 
+        <label htmlFor="firstName">First Name *</label>
+        <Field
+          type="text"
+          id="firstName"
           name="firstName"
-          value={formData.firstName || ''}
-          onChange={handleInputChange}
-          className="form-input" 
-          placeholder="Enter your first name" 
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="form-label">Last Name *</label>
-        <input 
-          type="text" 
-          name="lastName"
-          value={formData.lastName || ''}
-          onChange={handleInputChange}
-          className="form-input" 
-          placeholder="Enter your last name" 
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="form-label">Date of Birth *</label>
-        <input 
-          type="date" 
-          name="dateOfBirth"
-          value={formData.dateOfBirth || ''}
-          onChange={handleInputChange}
-          className="form-input" 
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="form-label">Gender *</label>
-        <select 
-          name="gender"
-          value={formData.gender || ''}
-          onChange={handleInputChange}
           className="form-input"
-        >
+        />
+        <ErrorMessage name="firstName" component="div" className="error" />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="lastName">Last Name *</label>
+        <Field
+          type="text"
+          id="lastName"
+          name="lastName"
+          className="form-input"
+        />
+        <ErrorMessage name="lastName" component="div" className="error" />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="dateOfBirth">Date of Birth *</label>
+        <Field
+          type="date"
+          id="dateOfBirth"
+          name="dateOfBirth"
+          className="form-input"
+        />
+        <ErrorMessage name="dateOfBirth" component="div" className="error" />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="gender">Gender *</label>
+        <Field as="select" id="gender" name="gender" className="form-input">
           <option value="">Select gender</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
           <option value="prefer-not-to-say">Prefer not to say</option>
-        </select>
+        </Field>
+        <ErrorMessage name="gender" component="div" className="error" />
       </div>
     </div>
   );
